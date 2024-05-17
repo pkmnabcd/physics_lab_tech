bb1ccafff0691140b66cdb99fad481b9cbcd9a00
pkmnabcd <42gdeckers@gmail.com>
2024-05-17 12:12:49 -0600

    Continuing homebrew origin development. Finished the file gathering. It turns out you can feed java files with similar names as with Python, including using ../ to go backwards. Made some changes to .gitignore .

--- homebrew origin/src/Main.java
+++ homebrew origin/src/Main.java
@@ -4,15 +4,23 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        File file = new File("");
+	    String filename = args[0];
+        File file = new File(filename);
+        ArrayList<String> lines = new ArrayList<String>();
+
         try (Scanner input = new Scanner(file)) {
-            if (input.hasNextLine()) {
-                input.nextLine();
+            while (input.hasNextLine()) {
+                String line = input.nextLine();
+                line = line.replace("\n", "").replace("\r", "");
+                lines.add(line);
             }
         } catch (java.io.FileNotFoundException e) {
             System.out.println("There was an error with opening the file:");
             System.out.println(e);
             System.exit(1);
+        }
+        for (String line : lines) {
+            System.out.println(line);
         }
     }
 }
