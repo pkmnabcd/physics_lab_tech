a16e29d3254b75fec466e30e511014460fbaf0ed
pkmnabcd <42gdeckers@gmail.com>
2024-05-28 13:00:50 -0600

    Finished implementing Parser.java . It seems to work with call good and with NaN data. Slightly modified Main to print the output of the parser and work correctly with the parser

--- homebrew origin/src/Main.java
+++ homebrew origin/src/Main.java
@@ -4,6 +4,14 @@ import java.lang.Double;
 public class Main {
     public static void main(String[] args) {
         String filename = args[0];
-        ArrayList<Double> inData = Parser.parseFile(filename);
+        ArrayList<ArrayList<Double>> inData = Parser.parseFile(filename);
+
+	for (ArrayList<Double> col : inData) {
+	    for (Double val : col) {
+	        System.out.print(val);
+		System.out.print(" ");
+	    }
+	    System.out.println();
+	}
     }
 }
--- homebrew origin/src/Parser.java
+++ homebrew origin/src/Parser.java
@@ -8,7 +8,7 @@ public class Parser {
      * This reads the data from the given file. If invalid data is given, null object is used instead of a Double object in that position in the output data.
      *  @param filename the path to and the name of the file that you want read.
      */
-    public static ArrayList<Double> parseFile(String filename) {
+    public static ArrayList<ArrayList<Double>> parseFile(String filename) {
         File file = new File(filename);
         ArrayList<String> lines = new ArrayList<String>();
 
@@ -24,10 +24,47 @@ public class Parser {
             System.exit(1);
         }
         
-        String header_line = lines.remove(0);
-	int line_len = header_line.length();
-	int col_len = 15;
+        String headerLine = lines.remove(0);
+	int lineLength = headerLine.length();
+	int colLength = 15;
+	int numberOfColumns = (int)(lineLength / colLength);
+	ArrayList<ArrayList<Double>> rowSet = new ArrayList<ArrayList<Double>>();
 
-	return new ArrayList<Double>();
+	for (String line : lines) {
+	    ArrayList<Double> lineData = new ArrayList<Double>();
+	    for (int i = 0; i < numberOfColumns; i++) {
+	        int indexStart = i * colLength;
+		int indexEnd = indexStart + colLength;
+		String substring = line.substring(indexStart, indexEnd);
+		substring = substring.replace(" ", "");
+		if (substring == "***************") {
+		    lineData.add(null);
+		} else if (substring.contains("NaN")) {
+		    lineData.add(null);
+		} else {
+		    lineData.add(Double.valueOf(substring));
+		}
+            }
+	    rowSet.add(lineData);
+	}
+
+        ArrayList<ArrayList<Double>> colSet = rowSetToColSet(rowSet);
+
+	return colSet; 
+    }
+    public static ArrayList<ArrayList<Double>> rowSetToColSet(ArrayList<ArrayList<Double>> rowSet) {
+        int rowCount = rowSet.size();
+	int colCount = rowSet.get(0).size();
+	ArrayList<ArrayList<Double>> colSet = new ArrayList<>();
+
+	for (int j = 0; j < colCount; j++) {
+	    colSet.add(new ArrayList<Double>());
+	}
+	for (int i = 0; i < rowCount; i++) {
+	    for (int j = 0; j < colCount; j++) {
+	        colSet.get(j).add(rowSet.get(i).get(j));
+	    }
+	}
+	return colSet;
     }
 }
