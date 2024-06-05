6e26e8b9a7667fbf891311bb1b6a5dd81dc4800c
pkmnabcd <42gdeckers@gmail.com>
2024-06-04 21:21:20 -0600

    Updated .gitignore to include files called temp_note.txt . Updated StandardDeviationCleaner.java so that it looks at the standard deviations of P12, P14, and BG filters

--- java_code/homebrew origin/src/StandardDeviationCleaner.java
+++ java_code/homebrew origin/src/StandardDeviationCleaner.java
@@ -1,11 +1,38 @@
 import java.util.ArrayList;
 public class StandardDeviationCleaner extends AbstractCleaner {
     public ArrayList<Integer> runCleaningAlgorithm(ArrayList<ArrayList<Double>> data) {
-	int index = 4;  // 4 is the P12 col index
-	double mean = getMean(data.get(index));
-	double stdDev = getStdDev(data.get(index), mean);
-	ArrayList<Integer> toRemove = getIndexesToRemove(data.get(index), mean, stdDev);
+	ArrayList<Integer> p12ToRemove = getToRemove(data.get(4));  // 4 is the P12 col index
+	ArrayList<Integer> p14ToRemove = getToRemove(data.get(5));  // 5 is the P14 col index
+	ArrayList<Integer> bgToRemove = getToRemove(data.get(6));  // 6 is the BG col index
+
+	ArrayList<Integer> combinedToRemove = combineToRemove(p12ToRemove, p14ToRemove, bgToRemove);
+	return combinedToRemove;
+    }
+    private ArrayList<Integer> getToRemove(ArrayList<Double> column) {
+	double mean = getMean(column);
+	double stdDev = getStdDev(column, mean);
+	ArrayList<Integer> toRemove = getIndexesToRemove(column, mean, stdDev);
 	return toRemove;
+    }
+    private ArrayList<Integer> combineToRemove(ArrayList<Integer> toRemove1, ArrayList<Integer> toRemove2, ArrayList<Integer> toRemove3) {
+	ArrayList<Integer> toRemoveOutput = toRemove1;
+	for (Integer val : toRemove2) {
+	    if (! toRemoveOutput.contains(val)) insertIndex(toRemoveOutput, val);
+	}
+	for (Integer val : toRemove3) {
+	    if (! toRemoveOutput.contains(val)) insertIndex(toRemoveOutput, val);
+	}
+	return toRemoveOutput;
+    }
+    private void insertIndex(ArrayList<Integer> toRemove, Integer val) {
+	int addIndex = -1;
+	for (int i = 0; i < toRemove.size(); i++) {
+	    if (toRemove.get(i) > val) {
+		addIndex = i;
+		break;
+	    }
+	}
+	toRemove.add(addIndex, val);
     }
     private double getMean(ArrayList<Double> column) {
 	int valCount = column.size();
