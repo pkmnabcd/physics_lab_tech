ce0a36148b29acbe4fd7799056738d308be9c53c
pkmnabcd <42gdeckers@gmail.com>
2024-06-10 11:43:07 -0600

    Fixed a major bug in the mainScript.sh where no inputs with spaces worked. Spaces now work with mainScript.bat

--- multi_platform_projects/homebrew origin/mainScript.bat
+++ multi_platform_projects/homebrew origin/mainScript.bat
@@ -1,21 +1,23 @@
 @echo off
-setlocal enabledelayedexpansion
 
 SET INPUT=%1
-SET JAVA_PATH=java_code\homebrew origin\src
-SET PYTHON_SCRIPT=python_code\data analysis\homebrew origin\main.py
+SET JAVA_PATH=java_src
+SET PYTHON_SCRIPT=python_src\main.py
 echo Java class files should be found at: %JAVA_PATH%
 echo Python script should be found at: %PYTHON_SCRIPT%
+echo The following was inputted: %INPUT%
 echo.
 
 echo Running the cleaning algorithm
-java -classpath "%JAVA_PATH%" Main "%INPUT%"
+java -classpath "%JAVA_PATH%" Main %INPUT%
 
 echo.
 echo Graphing the original data
-python "%PYTHON_SCRIPT%" "%INPUT%"
+python "%PYTHON_SCRIPT%" %INPUT%
 
+setlocal enabledelayedexpansion
 SET EDITED_INPUT=!INPUT:~0,-5!e.dat
+setlocal disabledelayedexpansion
 echo.
 echo Graphing the edited data
-python "%PYTHON_SCRIPT%" "%EDITED_INPUT%"
+python "%PYTHON_SCRIPT%" %EDITED_INPUT%
