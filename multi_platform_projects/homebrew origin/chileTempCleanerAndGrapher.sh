97a57938bd300a5488071ff51e60ed2662665ea3
pkmnabcd <42gdeckers@gmail.com>
2024-06-12 10:35:34 -0600

    Moved the bash script to a new name that matches the new batch script, and made it so they work the same. Also added to the BG stdev filter, having different levels of aggression when filtering out the data when there is twilight at the beginning AND end, twilight only at one end, or no twilight present that night.

--- multi_platform_projects/homebrew origin/chileTempCleanerAndGrapher.sh
+++ multi_platform_projects/homebrew origin/chileTempCleanerAndGrapher.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
 INPUT=$1
-JAVA_PATH="java_code/homebrew origin/src/"
-PYTHON_SCRIPT="python_code/data analysis/homebrew origin/main.py"
+JAVA_PATH="java_src"
+PYTHON_SCRIPT="python_src/main.py"
 echo "Java class files should be found at: $JAVA_PATH"
 echo "Python script should be found at: $PYTHON_SCRIPT"
-
+echo "The following was inputted: $INPUT"
 echo
+
 echo "Running the cleaning algorithm"
 java -cp "$JAVA_PATH" Main "$INPUT"
 echo
+
 echo "Graphing the original data"
 python "$PYTHON_SCRIPT" "$INPUT"
-EDITED_INPUT="${INPUT%.dat}e.dat"
 echo
+echo
+
+EDITED_INPUT="${INPUT%.dat}e.dat"
 echo "Graphing the edited data"
+echo "Edited input file: $EDITED_INPUT"
 python "$PYTHON_SCRIPT" "$EDITED_INPUT"
+
+echo
+echo
--- multi_platform_projects/homebrew origin/java_src/StandardDeviationCleanerBGOnly.java
+++ multi_platform_projects/homebrew origin/java_src/StandardDeviationCleanerBGOnly.java
@@ -8,8 +8,23 @@ public class StandardDeviationCleanerBGOnly extends StandardDeviationCleaner {
     }
     @Override
     protected ArrayList<Integer> getIndexesToRemove(ArrayList<Double> column, double mean, double stdDev) {
-	double upperLimit = mean;
-	double lowerLimit = mean - stdDev;
+	double upperLimit;
+	double lowerLimit;
+	boolean twilightAtStart = twilightAtBeginning(column);
+	boolean twilightAtFinish = twilightAtEnd(column);
+	if (twilightAtStart && twilightAtFinish) {
+	    System.out.println("Aggressive bounds used.");
+	    upperLimit = mean;
+	    lowerLimit = mean - stdDev;
+	} else if (twilightAtStart || twilightAtFinish) {
+	    System.out.println("Medium aggressive bounds used.");
+	    upperLimit = mean + (0.5 * stdDev);
+	    lowerLimit = mean - stdDev;
+	} else {
+	    System.out.println("No twilight detected. Large bounds used.");
+	    upperLimit = mean + (2 * stdDev);
+	    lowerLimit = mean - (2 * stdDev);
+	}
 	System.out.printf("Upper Limit: %f\nLower Limit: %f\n\n", upperLimit, lowerLimit);
 	ArrayList<Integer> indexes = new ArrayList<Integer>();
 	for (int i = 0; i < column.size(); i++) {
@@ -19,5 +34,19 @@ public class StandardDeviationCleanerBGOnly extends StandardDeviationCleaner {
 	    }
 	}
 	return indexes;
+    }
+    private boolean twilightAtBeginning(ArrayList<Double> column) {
+	if (column.get(0) > 50000) {
+	    System.out.println("Twilight detected at the beginning.");
+	    return true;
+	}
+	return false;
+    }
+    private boolean twilightAtEnd(ArrayList<Double> column) {
+	if (column.get(column.size() - 1) > 50000) {
+	    System.out.println("Twilight detected at the end.");
+	    return true;
+	}
+	return false;
     }
 }
