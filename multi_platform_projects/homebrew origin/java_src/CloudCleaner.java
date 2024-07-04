3be603a2582c1db3d9a79451e980c8200220c00a
pkmnabcd <42gdeckers@gmail.com>
2024-07-04 12:45:51 -0600

    Made different bounds in CloudCleaner to target cloudy days

--- multi_platform_projects/homebrew origin/java_src/CloudCleaner.java
+++ multi_platform_projects/homebrew origin/java_src/CloudCleaner.java
@@ -21,8 +21,20 @@ public class CloudCleaner extends AbstractCleaner {
 	double mean = STDEV.getMean(residualData, new ArrayList<Integer>());
 	double standardDev = STDEV.getStdDev(residualData, mean, new ArrayList<Integer>());
 	System.out.printf("Mean: %f\nStandardDeviation: %f\n\n", mean, standardDev);
-	double upperBound = mean + (2 * standardDev);
-	double lowerBound = mean - (2 * standardDev);
+	double upperBound, lowerBound;
+	if (standardDev > 20) {
+	    System.out.println("Aggressive bounds used.");
+	    upperBound = mean + (0.75 * standardDev);
+	    lowerBound = mean - (0.75 * standardDev);
+	} else if (standardDev > 5) {
+	    System.out.println("Medium bounds used.");
+	    upperBound = mean + (1 * standardDev);
+	    lowerBound = mean - (1 * standardDev);
+	} else {
+	    System.out.println("Lenient bounds used.");
+	    upperBound = mean + (2 * standardDev);
+	    lowerBound = mean - (2 * standardDev);
+	}
 
 	ArrayList<Integer> toRemove = new ArrayList<Integer>();
 	int resDataIndex = 0;
