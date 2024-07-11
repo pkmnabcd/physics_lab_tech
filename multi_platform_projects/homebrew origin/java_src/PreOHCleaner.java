fbbdc502f625c9b2a0cee52796d50ca2293bef69
pkmnabcd <42gdeckers@gmail.com>
2024-07-11 12:08:19 -0600

    Made the bounds for the precleaner more extreme based on the advice of my advisor.

--- multi_platform_projects/homebrew origin/java_src/PreOHCleaner.java
+++ multi_platform_projects/homebrew origin/java_src/PreOHCleaner.java
@@ -7,7 +7,7 @@ public class PreOHCleaner extends AbstractCleaner {
 	for (int i = 0; i < OHData.size(); i++) {
 	    if (alreadyRemoved.contains(i)) continue;
 	    if (OHData.get(i) == null) toRemove.add(i);
-	    else if (OHData.get(i) > 300) toRemove.add(i);
+	    else if (OHData.get(i) > 250) toRemove.add(i);
 	    else if (OHData.get(i) < 0) toRemove.add(i);
 	}
 	return toRemove;
