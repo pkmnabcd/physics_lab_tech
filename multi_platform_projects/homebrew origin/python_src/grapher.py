6a68a3221073c1f4b726dc4965bad2704f27c23e
Gabe Decker <42gdeckers@gmail.com>
2024-08-09 11:30:22 -0600

    After testing it out, finished cleaning up the old classes.

--- multi_platform_projects/homebrew origin/python_src/grapher.py
+++ multi_platform_projects/homebrew origin/python_src/grapher.py
@@ -1,32 +1,6 @@
 import matplotlib.pyplot as plt
 
 
-class SingleGraph:
-    def __init__(self, datasets: dict):
-        if type(self) is SingleGraph:
-            raise NotImplementedError("SingleGraph is an abstract class and must be extended")
-        self.datasets = datasets
-
-
-class SingleGraph1Set(SingleGraph):
-    def __init__(self, data: list, data_type: str):
-        super().__init__({data_type: data})
-
-
-class SingleGraph4Sets(SingleGraph):
-    def __init__(self,
-                 data0: list, data_type0: str,
-                 data1: list, data_type1: str,
-                 data2: list, data_type2: str,
-                 data3: list, data_type3: str):
-        super().__init__({
-            data_type0: data0,
-            data_type1: data1,
-            data_type2: data2,
-            data_type3: data3
-        })
-
-
 class CombinedGraph:
     def __init__(self, datasets: dict, metadata: dict, x_axis: list):
         self.metadata = metadata
