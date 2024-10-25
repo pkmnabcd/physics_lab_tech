45bed891bcd04a51bf0bdfc3a4d88b5bdc816a4a
Gabe Decker <42gdeckers@gmail.com>
2024-10-25 15:33:56 -0600

    Added some code to get all the lines in each file.

--- cpp/chileIntensityPlotting/yearly/parsing.cpp
+++ cpp/chileIntensityPlotting/yearly/parsing.cpp
@@ -4,10 +4,19 @@
 
 #include <filesystem>
 #include <fstream>
+#include <iostream>
+#include <vector>
 
 OneDay parseOneDay(std::filesystem::path dayPath)
 {
     std::ifstream file = std::ifstream(dayPath);
+    std::vector<std::string> lines;
+    std::string line;
+    while (std::getline(file, line))
+    {
+        std::cout << line << std::endl;
+    }
+    file.close();
 
     auto tempReturn = std::vector<double>();
     return OneDay(tempReturn, tempReturn);
