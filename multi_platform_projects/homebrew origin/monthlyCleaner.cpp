d98bdd0f89b7570fc7cace7dce78b0ca86482071
Gabe Decker <42gdeckers@gmail.com>
2024-11-01 11:11:35 -0600

    Finished the monthlyCleaner.cpp file, tested on linux

--- multi_platform_projects/homebrew origin/monthlyCleaner.cpp
+++ multi_platform_projects/homebrew origin/monthlyCleaner.cpp
@@ -1,16 +1,30 @@
 #include <cstdlib>
 #include <iostream>
 #include <filesystem>
+#include <regex>
 
 // Complile with command:
 // g++ monthlyCleaner.cpp -o MonthlyCleaner -std=c++20 -Wall -Wextra -pedantic
 int main()
 {
     const std::filesystem::path currentDir = std::filesystem::current_path();
-    std::cout << currentDir.string() << std::endl;
+    const std::string pattern_text = "OH_Andover_ALO[0-9][0-9]day[0-9]{1,3}.dat";
+    auto regexpr = std::regex(pattern_text);
 
     for (auto const& dir_entry : std::filesystem::directory_iterator{currentDir})
     {
-         std::cout << dir_entry.path().string() << std::endl;
+        const std::string filename = dir_entry.path().filename().string();
+        if (std::regex_match(filename.begin(), filename.end(), regexpr))
+        {
+            std::cout << "Filename match: " << filename << std::endl;
+            #ifdef _WIN32
+                std::string command = ".\\chileTempCleanerAndGrapher.bat ";
+            #else
+                std::string command = "./chileTempCleanerAndGrapher.sh ";
+            #endif
+            command += filename;
+            std::cout << "Command: " << command << std::endl;
+            std::system(command.c_str());
+        }
     }
 }
