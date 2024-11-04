a2e1068f79f5b68ef6d291892f1fe6c719c1198f
Gabe Decker <42gdeckers@gmail.com>
2024-11-04 14:53:53 -0700

    Finished the parser, needs to be checked. Updated the c++ requirement to c++23 to use str.contains()

--- cpp/chileIntensityPlotting/yearly/parsing.cpp
+++ cpp/chileIntensityPlotting/yearly/parsing.cpp
@@ -3,6 +3,7 @@
 #include "OneDay.hpp"
 #include "strTool.hpp"
 
+#include <cmath>
 #include <filesystem>
 #include <fstream>
 #include <iostream>
@@ -15,12 +16,73 @@ OneDay parseOneDay(std::filesystem::path dayPath)
     std::ifstream file = std::ifstream(dayPath);
     std::vector<std::string> lines;
     std::string line;
+    std::string headerLine;
+    unsigned int lineNumber = 1;
     while (std::getline(file, line))
     {
         std::cout << line << std::endl;
+        if (lineNumber == 1)
+        {
+            headerLine = line;
+        }
+        else
+        {
+            lines.push_back(line);
+        }
+        std::cout << line << std::endl;
+        lineNumber++;
     }
+    // TODO: Make sure this Java yoinked code works
+    // Right now, it gets all the data from the file into arrays of rows of data. Change it to just add the needed data right into the two needed columns
+
+    unsigned int lineLength = headerLine.length();
+    unsigned int colLength = 15;
+    unsigned int numberOfColumns = lineLength / colLength;
+    if (numberOfColumns != 8)
+    {
+        std::cout << "Warning! Number of Columns is wrong for " << dayPath << "!\nExpected 8 and got " << numberOfColumns << std::endl;
+    }
+    std::vector<double> timeData = std::vector<double>();
+    std::vector<double> tempData = std::vector<double>();
+
+    for (std::string line : lines)
+    {
+        for (unsigned int i = 0; i < 2; i++) // Just want the first two columns
+        {
+            unsigned int indexStart = i * colLength;
+            std::string substring = line.substr(indexStart, colLength);
+
+            std::string::size_type spacePos = substring.find(" ");
+            while (spacePos != std::string::npos)
+            {
+                substring.erase(spacePos);
+                spacePos = substring.find(" ");
+            }
+            double addVal;
+            if (substring.contains("*"))
+            {
+                addVal = std::nan("");
+            }
+            else if (substring.contains("NaN"))
+            {
+                addVal = std::nan("");
+            }
+            else
+            {
+                addVal = std::stod(substring);
+            }
+            if (i == 0)
+            {
+                timeData.push_back(addVal);
+            }
+            else if (i == 1)
+            {
+                tempData.push_back(addVal);
+            }
+        }
+    }
+
     file.close();
 
-    auto tempReturn = std::vector<double>();
-    return OneDay(tempReturn, tempReturn, doy);
+    return OneDay(timeData, tempData, doy);
 }
