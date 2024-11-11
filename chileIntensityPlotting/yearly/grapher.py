8ce2840dfc87f874b854092b0c412e49166e3dee
Gabe Decker <42gdeckers@gmail.com>
2024-11-11 15:14:16 -0700

    Added the parsing code to the grapher

--- chileIntensityPlotting/yearly/grapher.py
+++ chileIntensityPlotting/yearly/grapher.py
@@ -1,6 +1,25 @@
 from sys import argv
+
+def readAverages(path):
+    file = open(path)
+    lines = file.readlines()
+    for i in range(len(lines)):
+        lines[i] = lines[i].strip("\n")
+
+    times = []
+    temps = []
+    for line in lines:
+        cols = line.split(",")
+        times.append(int(cols[0]))
+        temps.append(float(cols[1]))
+
+    return times, temps
+
 if __name__ == "__main__":
     if len(argv) == 1:
         print("USAGE: python[3] grapher.py path_to/[year]dailyAverages.csv")
         exit()
     averagesPath = argv[1]
+    times, temps = readAverages(averagesPath)
+    for i in range(len(times)):
+        print(times[i], ',', temps[i])
