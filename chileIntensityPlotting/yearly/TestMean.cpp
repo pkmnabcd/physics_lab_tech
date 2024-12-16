c6c911dede57951d07156a11fe44482ada1ec561
Gabe Decker <42gdeckers@gmail.com>
2024-12-16 13:36:01 -0700

    Added in tests to verify my function

--- chileIntensityPlotting/yearly/TestMean.cpp
+++ chileIntensityPlotting/yearly/TestMean.cpp
@@ -86,3 +86,531 @@ TEST(parseDoyFromFilename, GetsCorrectNumberDay1)
     filename = "OH_Andover_ALO24day1.dat";
     EXPECT_EQ(367, parseDoyFromFilename(filename));
 }
+
+TEST(getHourLength, worksFor2009)
+{
+    std::string year = "2009";
+
+    OneDay d1 = OneDay({}, {}, 2);
+    OneDay d2 = OneDay({}, {}, 100);
+    OneDay d3 = OneDay({}, {}, 200);
+    OneDay d4 = OneDay({}, {}, 280);
+    OneDay d5 = OneDay({}, {}, 281);
+    OneDay d6 = OneDay({}, {}, 282);
+    OneDay d7 = OneDay({}, {}, 300);
+    OneDay d8 = OneDay({}, {}, 318);
+    OneDay d9 = OneDay({}, {}, 319);
+    OneDay d10 = OneDay({}, {}, 320);
+    OneDay d11 = OneDay({}, {}, 363);
+    OneDay d12 = OneDay({}, {}, 365);
+    OneDay d13 = OneDay({}, {}, 1);
+
+    EXPECT_EQ(10, getHourLength(d1, year));
+    EXPECT_EQ(10, getHourLength(d2, year));
+    EXPECT_EQ(10, getHourLength(d3, year));
+    EXPECT_EQ(10, getHourLength(d4, year));
+    EXPECT_EQ(10, getHourLength(d5, year));
+    EXPECT_EQ(10, getHourLength(d6, year));
+    EXPECT_EQ(10, getHourLength(d7, year));
+    EXPECT_EQ(10, getHourLength(d8, year));
+    EXPECT_EQ(10, getHourLength(d9, year));
+    EXPECT_EQ(10, getHourLength(d10, year));
+    EXPECT_EQ(10, getHourLength(d11, year));
+    EXPECT_EQ(10, getHourLength(d12, year));
+    EXPECT_EQ(10, getHourLength(d13, year));
+}
+
+TEST(getHourLength, worksFor2010)
+{
+    std::string year = "2010";
+
+    OneDay d1 = OneDay({}, {}, 2);
+    OneDay d2 = OneDay({}, {}, 100);
+    OneDay d3 = OneDay({}, {}, 200);
+    OneDay d4 = OneDay({}, {}, 280);
+    OneDay d5 = OneDay({}, {}, 281);
+    OneDay d6 = OneDay({}, {}, 282);
+    OneDay d7 = OneDay({}, {}, 300);
+    OneDay d8 = OneDay({}, {}, 318);
+    OneDay d9 = OneDay({}, {}, 319);
+    OneDay d10 = OneDay({}, {}, 320);
+    OneDay d11 = OneDay({}, {}, 363);
+    OneDay d12 = OneDay({}, {}, 365);
+    OneDay d13 = OneDay({}, {}, 1);
+
+    EXPECT_EQ(10, getHourLength(d1, year));
+    EXPECT_EQ(10, getHourLength(d2, year));
+    EXPECT_EQ(10, getHourLength(d3, year));
+    EXPECT_EQ(10, getHourLength(d4, year));
+    EXPECT_EQ(10, getHourLength(d5, year));
+    EXPECT_EQ(10, getHourLength(d6, year));
+    EXPECT_EQ(10, getHourLength(d7, year));
+    EXPECT_EQ(10, getHourLength(d8, year));
+    EXPECT_EQ(10, getHourLength(d9, year));
+    EXPECT_EQ(10, getHourLength(d10, year));
+    EXPECT_EQ(10, getHourLength(d11, year));
+    EXPECT_EQ(10, getHourLength(d12, year));
+    EXPECT_EQ(10, getHourLength(d13, year));
+}
+
+TEST(getHourLength, worksFor2011)
+{
+    std::string year = "2011";
+
+    OneDay d1 = OneDay({}, {}, 2);
+    OneDay d2 = OneDay({}, {}, 100);
+    OneDay d3 = OneDay({}, {}, 200);
+    OneDay d4 = OneDay({}, {}, 280);
+    OneDay d5 = OneDay({}, {}, 281);
+    OneDay d6 = OneDay({}, {}, 282);
+    OneDay d7 = OneDay({}, {}, 300);
+    OneDay d8 = OneDay({}, {}, 318);
+    OneDay d9 = OneDay({}, {}, 319);
+    OneDay d10 = OneDay({}, {}, 320);
+    OneDay d11 = OneDay({}, {}, 363);
+    OneDay d12 = OneDay({}, {}, 365);
+    OneDay d13 = OneDay({}, {}, 1);
+
+    EXPECT_EQ(10, getHourLength(d1, year));
+    EXPECT_EQ(10, getHourLength(d2, year));
+    EXPECT_EQ(10, getHourLength(d3, year));
+    EXPECT_EQ(10, getHourLength(d4, year));
+    EXPECT_EQ(10, getHourLength(d5, year));
+    EXPECT_EQ(10, getHourLength(d6, year));
+    EXPECT_EQ(10, getHourLength(d7, year));
+    EXPECT_EQ(10, getHourLength(d8, year));
+    EXPECT_EQ(33, getHourLength(d9, year));
+    EXPECT_EQ(33, getHourLength(d10, year));
+    EXPECT_EQ(33, getHourLength(d11, year));
+    EXPECT_EQ(33, getHourLength(d12, year));
+    EXPECT_EQ(33, getHourLength(d13, year));
+}
+
+TEST(getHourLength, worksFor2012)
+{
+    std::string year = "2012";
+
+    OneDay d1 = OneDay({}, {}, 2);
+    OneDay d2 = OneDay({}, {}, 100);
+    OneDay d3 = OneDay({}, {}, 200);
+    OneDay d4 = OneDay({}, {}, 280);
+    OneDay d5 = OneDay({}, {}, 281);
+    OneDay d6 = OneDay({}, {}, 282);
+    OneDay d7 = OneDay({}, {}, 300);
+    OneDay d8 = OneDay({}, {}, 318);
+    OneDay d9 = OneDay({}, {}, 319);
+    OneDay d10 = OneDay({}, {}, 320);
+    OneDay d11 = OneDay({}, {}, 363);
+    OneDay d12 = OneDay({}, {}, 365);
+    OneDay d13 = OneDay({}, {}, 1);
+
+    EXPECT_EQ(33, getHourLength(d1, year));
+    EXPECT_EQ(33, getHourLength(d2, year));
+    EXPECT_EQ(33, getHourLength(d3, year));
+    EXPECT_EQ(33, getHourLength(d4, year));
+    EXPECT_EQ(33, getHourLength(d5, year));
+    EXPECT_EQ(33, getHourLength(d6, year));
+    EXPECT_EQ(33, getHourLength(d7, year));
+    EXPECT_EQ(33, getHourLength(d8, year));
+    EXPECT_EQ(33, getHourLength(d9, year));
+    EXPECT_EQ(33, getHourLength(d10, year));
+    EXPECT_EQ(33, getHourLength(d11, year));
+    EXPECT_EQ(33, getHourLength(d12, year));
+    EXPECT_EQ(33, getHourLength(d13, year));
+}
+
+TEST(getHourLength, worksFor2013)
+{
+    std::string year = "2013";
+
+    OneDay d1 = OneDay({}, {}, 2);
+    OneDay d2 = OneDay({}, {}, 100);
+    OneDay d3 = OneDay({}, {}, 200);
+    OneDay d4 = OneDay({}, {}, 280);
+    OneDay d5 = OneDay({}, {}, 281);
+    OneDay d6 = OneDay({}, {}, 282);
+    OneDay d7 = OneDay({}, {}, 300);
+    OneDay d8 = OneDay({}, {}, 318);
+    OneDay d9 = OneDay({}, {}, 319);
+    OneDay d10 = OneDay({}, {}, 320);
+    OneDay d11 = OneDay({}, {}, 363);
+    OneDay d12 = OneDay({}, {}, 365);
+    OneDay d13 = OneDay({}, {}, 1);
+
+    EXPECT_EQ(33, getHourLength(d1, year));
+    EXPECT_EQ(33, getHourLength(d2, year));
+    EXPECT_EQ(33, getHourLength(d3, year));
+    EXPECT_EQ(33, getHourLength(d4, year));
+    EXPECT_EQ(33, getHourLength(d5, year));
+    EXPECT_EQ(33, getHourLength(d6, year));
+    EXPECT_EQ(33, getHourLength(d7, year));
+    EXPECT_EQ(33, getHourLength(d8, year));
+    EXPECT_EQ(33, getHourLength(d9, year));
+    EXPECT_EQ(33, getHourLength(d10, year));
+    EXPECT_EQ(33, getHourLength(d11, year));
+    EXPECT_EQ(33, getHourLength(d12, year));
+    EXPECT_EQ(33, getHourLength(d13, year));
+}
+
+TEST(getHourLength, worksFor2014)
+{
+    std::string year = "2014";
+
+    OneDay d1 = OneDay({}, {}, 2);
+    OneDay d2 = OneDay({}, {}, 100);
+    OneDay d3 = OneDay({}, {}, 200);
+    OneDay d4 = OneDay({}, {}, 280);
+    OneDay d5 = OneDay({}, {}, 281);
+    OneDay d6 = OneDay({}, {}, 282);
+    OneDay d7 = OneDay({}, {}, 300);
+    OneDay d8 = OneDay({}, {}, 318);
+    OneDay d9 = OneDay({}, {}, 319);
+    OneDay d10 = OneDay({}, {}, 320);
+    OneDay d11 = OneDay({}, {}, 363);
+    OneDay d12 = OneDay({}, {}, 365);
+    OneDay d13 = OneDay({}, {}, 1);
+
+    EXPECT_EQ(33, getHourLength(d1, year));
+    EXPECT_EQ(33, getHourLength(d2, year));
+    EXPECT_EQ(33, getHourLength(d3, year));
+    EXPECT_EQ(33, getHourLength(d4, year));
+    EXPECT_EQ(33, getHourLength(d5, year));
+    EXPECT_EQ(33, getHourLength(d6, year));
+    EXPECT_EQ(33, getHourLength(d7, year));
+    EXPECT_EQ(33, getHourLength(d8, year));
+    EXPECT_EQ(33, getHourLength(d9, year));
+    EXPECT_EQ(33, getHourLength(d10, year));
+    EXPECT_EQ(33, getHourLength(d11, year));
+    EXPECT_EQ(33, getHourLength(d12, year));
+    EXPECT_EQ(33, getHourLength(d13, year));
+}
+
+TEST(getHourLength, worksFor2015)
+{
+    std::string year = "2015";
+
+    OneDay d1 = OneDay({}, {}, 2);
+    OneDay d2 = OneDay({}, {}, 100);
+    OneDay d3 = OneDay({}, {}, 200);
+    OneDay d4 = OneDay({}, {}, 280);
+    OneDay d5 = OneDay({}, {}, 281);
+    OneDay d6 = OneDay({}, {}, 282);
+    OneDay d7 = OneDay({}, {}, 300);
+    OneDay d8 = OneDay({}, {}, 318);
+    OneDay d9 = OneDay({}, {}, 319);
+    OneDay d10 = OneDay({}, {}, 320);
+    OneDay d11 = OneDay({}, {}, 363);
+    OneDay d12 = OneDay({}, {}, 365);
+    OneDay d13 = OneDay({}, {}, 1);
+
+    EXPECT_EQ(33, getHourLength(d1, year));
+    EXPECT_EQ(33, getHourLength(d2, year));
+    EXPECT_EQ(33, getHourLength(d3, year));
+    EXPECT_EQ(33, getHourLength(d4, year));
+    EXPECT_EQ(33, getHourLength(d5, year));
+    EXPECT_EQ(33, getHourLength(d6, year));
+    EXPECT_EQ(33, getHourLength(d7, year));
+    EXPECT_EQ(33, getHourLength(d8, year));
+    EXPECT_EQ(33, getHourLength(d9, year));
+    EXPECT_EQ(33, getHourLength(d10, year));
+    EXPECT_EQ(33, getHourLength(d11, year));
+    EXPECT_EQ(33, getHourLength(d12, year));
+    EXPECT_EQ(33, getHourLength(d13, year));
+}
+
+TEST(getHourLength, worksFor2016)
+{
+    std::string year = "2016";
+
+    OneDay d1 = OneDay({}, {}, 2);
+    OneDay d2 = OneDay({}, {}, 100);
+    OneDay d3 = OneDay({}, {}, 200);
+    OneDay d4 = OneDay({}, {}, 280);
+    OneDay d5 = OneDay({}, {}, 281);
+    OneDay d6 = OneDay({}, {}, 282);
+    OneDay d7 = OneDay({}, {}, 300);
+    OneDay d8 = OneDay({}, {}, 318);
+    OneDay d9 = OneDay({}, {}, 319);
+    OneDay d10 = OneDay({}, {}, 320);
+    OneDay d11 = OneDay({}, {}, 363);
+    OneDay d12 = OneDay({}, {}, 365);
+    OneDay d13 = OneDay({}, {}, 1);
+
+    EXPECT_EQ(33, getHourLength(d1, year));
+    EXPECT_EQ(33, getHourLength(d2, year));
+    EXPECT_EQ(33, getHourLength(d3, year));
+    EXPECT_EQ(33, getHourLength(d4, year));
+    EXPECT_EQ(33, getHourLength(d5, year));
+    EXPECT_EQ(33, getHourLength(d6, year));
+    EXPECT_EQ(33, getHourLength(d7, year));
+    EXPECT_EQ(33, getHourLength(d8, year));
+    EXPECT_EQ(33, getHourLength(d9, year));
+    EXPECT_EQ(33, getHourLength(d10, year));
+    EXPECT_EQ(33, getHourLength(d11, year));
+    EXPECT_EQ(33, getHourLength(d12, year));
+    EXPECT_EQ(33, getHourLength(d13, year));
+}
+
+TEST(getHourLength, worksFor2017)
+{
+    std::string year = "2017";
+
+    OneDay d1 = OneDay({}, {}, 2);
+    OneDay d2 = OneDay({}, {}, 100);
+    OneDay d3 = OneDay({}, {}, 200);
+    OneDay d4 = OneDay({}, {}, 280);
+    OneDay d5 = OneDay({}, {}, 281);
+    OneDay d6 = OneDay({}, {}, 282);
+    OneDay d7 = OneDay({}, {}, 300);
+    OneDay d8 = OneDay({}, {}, 318);
+    OneDay d9 = OneDay({}, {}, 319);
+    OneDay d10 = OneDay({}, {}, 320);
+    OneDay d11 = OneDay({}, {}, 363);
+    OneDay d12 = OneDay({}, {}, 365);
+    OneDay d13 = OneDay({}, {}, 1);
+
+    EXPECT_EQ(33, getHourLength(d1, year));
+    EXPECT_EQ(33, getHourLength(d2, year));
+    EXPECT_EQ(33, getHourLength(d3, year));
+    EXPECT_EQ(33, getHourLength(d4, year));
+    EXPECT_EQ(33, getHourLength(d5, year));
+    EXPECT_EQ(33, getHourLength(d6, year));
+    EXPECT_EQ(33, getHourLength(d7, year));
+    EXPECT_EQ(33, getHourLength(d8, year));
+    EXPECT_EQ(33, getHourLength(d9, year));
+    EXPECT_EQ(33, getHourLength(d10, year));
+    EXPECT_EQ(33, getHourLength(d11, year));
+    EXPECT_EQ(33, getHourLength(d12, year));
+    EXPECT_EQ(33, getHourLength(d13, year));
+}
+
+TEST(getHourLength, worksFor2018)
+{
+    std::string year = "2018";
+
+    OneDay d1 = OneDay({}, {}, 2);
+    OneDay d2 = OneDay({}, {}, 100);
+    OneDay d3 = OneDay({}, {}, 200);
+    OneDay d4 = OneDay({}, {}, 280);
+    OneDay d5 = OneDay({}, {}, 281);
+    OneDay d6 = OneDay({}, {}, 282);
+    OneDay d7 = OneDay({}, {}, 300);
+    OneDay d8 = OneDay({}, {}, 318);
+    OneDay d9 = OneDay({}, {}, 319);
+    OneDay d10 = OneDay({}, {}, 320);
+    OneDay d11 = OneDay({}, {}, 363);
+    OneDay d12 = OneDay({}, {}, 365);
+    OneDay d13 = OneDay({}, {}, 1);
+
+    EXPECT_EQ(33, getHourLength(d1, year));
+    EXPECT_EQ(33, getHourLength(d2, year));
+    EXPECT_EQ(33, getHourLength(d3, year));
+    EXPECT_EQ(33, getHourLength(d4, year));
+    EXPECT_EQ(33, getHourLength(d5, year));
+    EXPECT_EQ(33, getHourLength(d6, year));
+    EXPECT_EQ(33, getHourLength(d7, year));
+    EXPECT_EQ(33, getHourLength(d8, year));
+    EXPECT_EQ(33, getHourLength(d9, year));
+    EXPECT_EQ(33, getHourLength(d10, year));
+    EXPECT_EQ(33, getHourLength(d11, year));
+    EXPECT_EQ(33, getHourLength(d12, year));
+    EXPECT_EQ(33, getHourLength(d13, year));
+}
+
+TEST(getHourLength, worksFor2019)
+{
+    std::string year = "2019";
+
+    OneDay d1 = OneDay({}, {}, 2);
+    OneDay d2 = OneDay({}, {}, 100);
+    OneDay d3 = OneDay({}, {}, 200);
+    OneDay d4 = OneDay({}, {}, 280);
+    OneDay d5 = OneDay({}, {}, 281);
+    OneDay d6 = OneDay({}, {}, 282);
+    OneDay d7 = OneDay({}, {}, 300);
+    OneDay d8 = OneDay({}, {}, 318);
+    OneDay d9 = OneDay({}, {}, 319);
+    OneDay d10 = OneDay({}, {}, 320);
+    OneDay d11 = OneDay({}, {}, 363);
+    OneDay d12 = OneDay({}, {}, 365);
+    OneDay d13 = OneDay({}, {}, 1);
+
+    EXPECT_EQ(33, getHourLength(d1, year));
+    EXPECT_EQ(33, getHourLength(d2, year));
+    EXPECT_EQ(33, getHourLength(d3, year));
+    EXPECT_EQ(33, getHourLength(d4, year));
+    EXPECT_EQ(33, getHourLength(d5, year));
+    EXPECT_EQ(33, getHourLength(d6, year));
+    EXPECT_EQ(33, getHourLength(d7, year));
+    EXPECT_EQ(33, getHourLength(d8, year));
+    EXPECT_EQ(33, getHourLength(d9, year));
+    EXPECT_EQ(33, getHourLength(d10, year));
+    EXPECT_EQ(33, getHourLength(d11, year));
+    EXPECT_EQ(33, getHourLength(d12, year));
+    EXPECT_EQ(33, getHourLength(d13, year));
+}
+
+TEST(getHourLength, worksFor2020)
+{
+    std::string year = "2020";
+
+    OneDay d1 = OneDay({}, {}, 2);
+    OneDay d2 = OneDay({}, {}, 100);
+    OneDay d3 = OneDay({}, {}, 200);
+    OneDay d4 = OneDay({}, {}, 280);
+    OneDay d5 = OneDay({}, {}, 281);
+    OneDay d6 = OneDay({}, {}, 282);
+    OneDay d7 = OneDay({}, {}, 300);
+    OneDay d8 = OneDay({}, {}, 318);
+    OneDay d9 = OneDay({}, {}, 319);
+    OneDay d10 = OneDay({}, {}, 320);
+    OneDay d11 = OneDay({}, {}, 363);
+    OneDay d12 = OneDay({}, {}, 365);
+    OneDay d13 = OneDay({}, {}, 1);
+
+    EXPECT_EQ(33, getHourLength(d1, year));
+    EXPECT_EQ(33, getHourLength(d2, year));
+    EXPECT_EQ(33, getHourLength(d3, year));
+    EXPECT_EQ(33, getHourLength(d4, year));
+    EXPECT_EQ(33, getHourLength(d5, year));
+    EXPECT_EQ(33, getHourLength(d6, year));
+    EXPECT_EQ(33, getHourLength(d7, year));
+    EXPECT_EQ(33, getHourLength(d8, year));
+    EXPECT_EQ(33, getHourLength(d9, year));
+    EXPECT_EQ(33, getHourLength(d10, year));
+    EXPECT_EQ(33, getHourLength(d11, year));
+    EXPECT_EQ(33, getHourLength(d12, year));
+    EXPECT_EQ(33, getHourLength(d13, year));
+}
+
+TEST(getHourLength, worksFor2021)
+{
+    std::string year = "2021";
+
+    OneDay d1 = OneDay({}, {}, 2);
+    OneDay d2 = OneDay({}, {}, 100);
+    OneDay d3 = OneDay({}, {}, 200);
+    OneDay d4 = OneDay({}, {}, 280);
+    OneDay d5 = OneDay({}, {}, 281);
+    OneDay d6 = OneDay({}, {}, 282);
+    OneDay d7 = OneDay({}, {}, 300);
+    OneDay d8 = OneDay({}, {}, 318);
+    OneDay d9 = OneDay({}, {}, 319);
+    OneDay d10 = OneDay({}, {}, 320);
+    OneDay d11 = OneDay({}, {}, 363);
+    OneDay d12 = OneDay({}, {}, 365);
+    OneDay d13 = OneDay({}, {}, 1);
+
+    EXPECT_EQ(33, getHourLength(d1, year));
+    EXPECT_EQ(33, getHourLength(d2, year));
+    EXPECT_EQ(33, getHourLength(d3, year));
+    EXPECT_EQ(33, getHourLength(d4, year));
+    EXPECT_EQ(33, getHourLength(d5, year));
+    EXPECT_EQ(33, getHourLength(d6, year));
+    EXPECT_EQ(33, getHourLength(d7, year));
+    EXPECT_EQ(33, getHourLength(d8, year));
+    EXPECT_EQ(33, getHourLength(d9, year));
+    EXPECT_EQ(33, getHourLength(d10, year));
+    EXPECT_EQ(33, getHourLength(d11, year));
+    EXPECT_EQ(33, getHourLength(d12, year));
+    EXPECT_EQ(33, getHourLength(d13, year));
+}
+
+TEST(getHourLength, worksFor2022)
+{
+    std::string year = "2022";
+
+    OneDay d1 = OneDay({}, {}, 2);
+    OneDay d2 = OneDay({}, {}, 100);
+    OneDay d3 = OneDay({}, {}, 200);
+    OneDay d4 = OneDay({}, {}, 280);
+    OneDay d5 = OneDay({}, {}, 281);
+    OneDay d6 = OneDay({}, {}, 282);
+    OneDay d7 = OneDay({}, {}, 300);
+    OneDay d8 = OneDay({}, {}, 318);
+    OneDay d9 = OneDay({}, {}, 319);
+    OneDay d10 = OneDay({}, {}, 320);
+    OneDay d11 = OneDay({}, {}, 363);
+    OneDay d12 = OneDay({}, {}, 365);
+    OneDay d13 = OneDay({}, {}, 1);
+
+    EXPECT_EQ(33, getHourLength(d1, year));
+    EXPECT_EQ(33, getHourLength(d2, year));
+    EXPECT_EQ(33, getHourLength(d3, year));
+    EXPECT_EQ(33, getHourLength(d4, year));
+    EXPECT_EQ(33, getHourLength(d5, year));
+    EXPECT_EQ(33, getHourLength(d6, year));
+    EXPECT_EQ(33, getHourLength(d7, year));
+    EXPECT_EQ(33, getHourLength(d8, year));
+    EXPECT_EQ(33, getHourLength(d9, year));
+    EXPECT_EQ(33, getHourLength(d10, year));
+    EXPECT_EQ(33, getHourLength(d11, year));
+    EXPECT_EQ(33, getHourLength(d12, year));
+    EXPECT_EQ(33, getHourLength(d13, year));
+}
+
+TEST(getHourLength, worksFor2023)
+{
+    std::string year = "2023";
+
+    OneDay d1 = OneDay({}, {}, 2);
+    OneDay d2 = OneDay({}, {}, 100);
+    OneDay d3 = OneDay({}, {}, 200);
+    OneDay d4 = OneDay({}, {}, 280);
+    OneDay d5 = OneDay({}, {}, 281);
+    OneDay d6 = OneDay({}, {}, 282);
+    OneDay d7 = OneDay({}, {}, 300);
+    OneDay d8 = OneDay({}, {}, 318);
+    OneDay d9 = OneDay({}, {}, 319);
+    OneDay d10 = OneDay({}, {}, 320);
+    OneDay d11 = OneDay({}, {}, 363);
+    OneDay d12 = OneDay({}, {}, 365);
+    OneDay d13 = OneDay({}, {}, 1);
+
+    EXPECT_EQ(33, getHourLength(d1, year));
+    EXPECT_EQ(33, getHourLength(d2, year));
+    EXPECT_EQ(33, getHourLength(d3, year));
+    EXPECT_EQ(33, getHourLength(d4, year));
+    EXPECT_EQ(10, getHourLength(d5, year));
+    EXPECT_EQ(10, getHourLength(d6, year));
+    EXPECT_EQ(10, getHourLength(d7, year));
+    EXPECT_EQ(10, getHourLength(d8, year));
+    EXPECT_EQ(10, getHourLength(d9, year));
+    EXPECT_EQ(10, getHourLength(d10, year));
+    EXPECT_EQ(10, getHourLength(d11, year));
+    EXPECT_EQ(10, getHourLength(d12, year));
+    EXPECT_EQ(10, getHourLength(d13, year));
+}
+
+TEST(getHourLength, worksFor2024)
+{
+    std::string year = "2024";
+
+    OneDay d1 = OneDay({}, {}, 2);
+    OneDay d2 = OneDay({}, {}, 100);
+    OneDay d3 = OneDay({}, {}, 200);
+    OneDay d4 = OneDay({}, {}, 280);
+    OneDay d5 = OneDay({}, {}, 281);
+    OneDay d6 = OneDay({}, {}, 282);
+    OneDay d7 = OneDay({}, {}, 300);
+    OneDay d8 = OneDay({}, {}, 318);
+    OneDay d9 = OneDay({}, {}, 319);
+    OneDay d10 = OneDay({}, {}, 320);
+    OneDay d11 = OneDay({}, {}, 363);
+    OneDay d12 = OneDay({}, {}, 365);
+    OneDay d13 = OneDay({}, {}, 1);
+
+    EXPECT_EQ(10, getHourLength(d1, year));
+    EXPECT_EQ(10, getHourLength(d2, year));
+    EXPECT_EQ(10, getHourLength(d3, year));
+    EXPECT_EQ(10, getHourLength(d4, year));
+    EXPECT_EQ(10, getHourLength(d5, year));
+    EXPECT_EQ(10, getHourLength(d6, year));
+    EXPECT_EQ(10, getHourLength(d7, year));
+    EXPECT_EQ(10, getHourLength(d8, year));
+    EXPECT_EQ(10, getHourLength(d9, year));
+    EXPECT_EQ(10, getHourLength(d10, year));
+    EXPECT_EQ(10, getHourLength(d11, year));
+    EXPECT_EQ(10, getHourLength(d12, year));
+    EXPECT_EQ(10, getHourLength(d13, year));
+}
--- chileIntensityPlotting/yearly/mean.hpp
+++ chileIntensityPlotting/yearly/mean.hpp
@@ -2,6 +2,7 @@
 
 #include "OneDay.hpp"
 
+#include <cstdint>
 #include <optional>
 #include <string>
 #include <vector>
@@ -9,6 +10,8 @@ std::vector<OneDay> getYearlyAverages(std::string year_path);
 
 std::optional<double> calculateAverage(OneDay dayData, bool doingTest, std::string year);
 double calculateStdDev(OneDay dayData);
+
+std::uint8_t getHourLength(OneDay day, std::string yearStr);
 
 const std::vector<std::string> MONTH_HEADERS = {
     "Jan", "Feb", "Mar", "Apr", "May", "Jun",
