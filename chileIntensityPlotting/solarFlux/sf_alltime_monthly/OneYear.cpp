8d4697a5e24d9cb1bb1dce5c959fb84fff749674
Gabe Decker <42gdeckers@gmail.com>
2025-03-25 12:48:02 -0600

    Finished the solar flux and OH monthly average getters. Starting to work on the stdev getter

--- chileIntensityPlotting/solarFlux/sf_alltime_monthly/OneYear.cpp
+++ chileIntensityPlotting/solarFlux/sf_alltime_monthly/OneYear.cpp
@@ -5,100 +5,131 @@
 #include <string>
 #include <vector>
 
-OneYear::OneYear(std::string year, std::vector<double> dailyOHAvg, std::vector<double> dailySfAvg, std::vector<std::uint8_t> ohMonths, std::vector<std::uint8_t> sfMonths) :
+OneYear::OneYear(std::string year, std::vector<double> dailyOHAvg, std::vector<double> dailySfAvg, std::vector<std::uint8_t> OHMonths, std::vector<std::uint8_t> sfMonths) :
     m_year(year),
     m_dailyOHAvg(dailyOHAvg),
     m_dailySfAvg(dailySfAvg),
-    m_ohMonths(ohMonths),
+    m_OHMonths(OHMonths),
     m_sfMonths(sfMonths)
 {
-    computeSaveAverage();
-    computeSaveStdDev();
-    computeMonths();
+    computeSaveOHAverage();
+    computeSaveSfAverage();
+    computeSaveOHStdDev();
+    computeSaveSfStdDev();
+    collapseOHMonths();
+    collapseSfMonths();
 }
 
-// TODO: Fix these functions to work with the vectors of averages and stdevs and compute them monthly
-void OneYear::computeSaveAverage()
+void OneYear::computeSaveOHAverage()
 {
-    // TODO: It occurred to me that the number of OH days is less than the number of sf days, so I'll have to have separate average computations. Will need sf months and oh months. Either make a new function to separate the sf and oh computations or do it all here.
-    std::uint8_t currentMonth = m_dailyMonths[0];
+    std::uint8_t currentMonth = m_OHMonths[0];
     std::uint8_t daysInMonth = 0;
-    double ohTotal = 0;
-    double sfTotal = 0;
-    std::uint16_t ohNanCount = 0;
-    std::uint16_t sfNanCount = 0;
-    for (std::uint16_t i = 0; i < m_dailyMonths.size(); i++)
+    double total = 0;
+    std::uint16_t nanCount = 0;
+    for (std::uint16_t i = 0; i < m_OHMonths.size(); i++)
     {
-        if (m_dailyMonths[i] == currentMonth)
+        if (m_OHMonths[i] == currentMonth)
         {
             daysInMonth++;
-            double ohVal = m_dailyOHAvg[i];
-            double sfVal = m_dailySolarAvg[i];
-            if (std::isnan(ohVal))
+            double val = m_dailyOHAvg[i];
+            if (std::isnan(val))
             {
-                ohNanCount++;
+                nanCount++;
             }
             else
             {
-                ohTotal += ohVal;
-            }
-            if (std::isnan(sfVal))
-            {
-                sfNanCount++;
-            }
-            else
-            {
-                ohTotal += sfVal;
+                total += val;
             }
         }
         else
         {
-            currentMonth = m_dailyMonths[i];
+            double avg = total / (daysInMonth - nanCount);
+            m_OHMonthlyAverages.push_back(avg);
+
+            currentMonth = m_OHMonths[i];
             daysInMonth = 0;
-            ohTotal = 0;
-            sfTotal = 0;
-            ohNanCount = 0;
-            sfNanCount = 0;
-
-            double ohAvg = ohTotal / (daysInMonth - ohNanCount);
-            double sfAvg = sfTotal / (daysInMonth - sfNanCount);
-            m_OHMonthlyAverages.push_back(ohAvg);
-            m_SolarMonthlyAverages.push_back(sfAvg);
+            total = 0;
+            nanCount = 0;
         }
-        // NOTE: Make sure to compute the last month's average and add it after the loop
     }
+    // NOTE: adding the last average
+    double avg = total / (daysInMonth - nanCount);
+    m_OHMonthlyAverages.push_back(avg);
+}
 
-    // NOTE: computing OH average
+void OneYear::computeSaveSfAverage()
+{
+    std::uint8_t currentMonth = m_sfMonths[0];
+    std::uint8_t daysInMonth = 0;
     double total = 0;
-    std::vector<double> temperature = m_dailyOHAvg;
     std::uint16_t nanCount = 0;
-    for (auto& val : temperature)
+    for (std::uint16_t i = 0; i < m_sfMonths.size(); i++)
     {
-        if (std::isnan(val))
+        if (m_sfMonths[i] == currentMonth)
         {
-            nanCount++;
-            continue;
+            daysInMonth++;
+            double val = m_dailySfAvg[i];
+            if (std::isnan(val))
+            {
+                nanCount++;
+            }
+            else
+            {
+                total += val;
+            }
         }
-        total += val;
-    }
-    double average = total / (temperature.size() - nanCount);
-    m_OHAverage = average;
+        else
+        {
+            double avg = total / (daysInMonth - nanCount);
+            m_sfMonthlyAverages.push_back(avg);
 
-    // NOTE: computing solar flux average
-    total = 0;
-    std::vector<double> solarFlux = m_dailySolarAvg;
-    nanCount = 0;
-    for (auto& val : solarFlux)
-    {
-        if (std::isnan(val))
-        {
-            nanCount++;
-            continue;
+            currentMonth = m_sfMonths[i];
+            daysInMonth = 0;
+            total = 0;
+            nanCount = 0;
         }
-        total += val;
     }
-    average = total / (solarFlux.size() - nanCount);
-    m_SolarAverage = average;
+    // NOTE: adding the last average
+    double avg = total / (daysInMonth - nanCount);
+    m_sfMonthlyAverages.push_back(avg);
+}
+
+void OneYear::computeSaveOHStdDev()
+{
+    std::uint8_t monthIndex = 0;
+    std::uint8_t currentMonth = m_OHMonths[i];
+    std::uint8_t daysInMonth = 0;
+    double summation = 0;
+    std::uint16_t nanCount = 0;
+    for (std::uint16_t i = 0; i < m_OHMonths.size(); i++)
+    {
+        if (m_OHMonths[i] == currentMonth)
+        {
+            daysInMonth++;
+            double val = m_dailyOHAvg[i];
+            if (std::isnan(val))
+            {
+                nanCount++;
+            }
+            else
+            {
+                total += val;
+            }
+        }
+        else
+        {
+            double avg = total / (daysInMonth - nanCount);
+            m_sfMonthlyAverages.push_back(avg);
+
+            currentMonth = m_sfMonths[i];
+            daysInMonth = 0;
+            total = 0;
+            nanCount = 0;
+        }
+    }
+    // NOTE: adding the last average
+    double avg = total / (daysInMonth - nanCount);
+    m_sfMonthlyAverages.push_back(avg);
 }
 
 void OneYear::computeSaveStdDev()
@@ -146,13 +177,12 @@ void OneYear::computeSaveStdDev()
     m_SolarStdDev = stdDev;
 }
 
-// TODO: Change this so that it uses the sf months since those are more consistent
-void OneYear::computeMonths()
+void OneYear::collapseOHMonths()
 {
-    for (std::uint8_t& currentMonth : m_dailyMonths)
+    for (std::uint8_t& currentMonth : m_OHMonths)
     {
         bool present = false;
-        for (std::uint8_t& month : m_months)
+        for (std::uint8_t& month : m_collapsedOHMonths)
         {
             if (month == currentMonth)
             {
@@ -162,7 +192,27 @@ void OneYear::computeMonths()
         }
         if (!present)
         {
-            m_months.push_back(currentMonth);
+            m_collapsedOHMonths.push_back(currentMonth);
+        }
+    }
+}
+
+void OneYear::collapseSfMonths()
+{
+    for (std::uint8_t& currentMonth : m_sfMonths)
+    {
+        bool present = false;
+        for (std::uint8_t& month : m_collapsedSfMonths)
+        {
+            if (month == currentMonth)
+            {
+                present = true;
+                break;
+            }
+        }
+        if (!present)
+        {
+            m_collapsedSfMonths.push_back(currentMonth);
         }
     }
 }
--- chileIntensityPlotting/solarFlux/sf_alltime_monthly/OneYear.hpp
+++ chileIntensityPlotting/solarFlux/sf_alltime_monthly/OneYear.hpp
@@ -7,13 +7,14 @@
 class OneYear
 {
   public:
-    OneYear(std::string year, std::vector<double> dailyOHAvg, std::vector<double> dailySfAvg, std::vector<std::uint8_t> ohMonths, std::vector<std::uint8_t> sfMonths);
+    OneYear(std::string year, std::vector<double> dailyOHAvg, std::vector<double> dailySfAvg, std::vector<std::uint8_t> OHMonths, std::vector<std::uint8_t> sfMonths);
 
     std::string getYear() { return m_year; }
     std::vector<double> getDailyOHAvg() { return m_dailyOHAvg; }
     std::vector<double> getDailySfAvg() { return m_dailySfAvg; }
-    std::vector<std::uint8_t> getMonths() { return m_months; }
-    std::vector<std::uint8_t> getOhMonths() { return m_ohMonths; }
+    std::vector<std::uint8_t> getCollapsedOHMonths() { return m_collapsedOHMonths; }
+    std::vector<std::uint8_t> getCollapsedSfMonths() { return m_collapsedSfMonths; }
+    std::vector<std::uint8_t> getOHMonths() { return m_OHMonths; }
     std::vector<std::uint8_t> getSfMonths() { return m_sfMonths; }
 
     std::vector<double> getOHAverage() { return m_OHMonthlyAverages; }
@@ -25,10 +26,11 @@ class OneYear
     const std::string m_year;
     std::vector<double> m_dailyOHAvg;
     std::vector<double> m_dailySfAvg;
-    std::vector<std::uint8_t> m_ohMonths;
+    std::vector<std::uint8_t> m_OHMonths;
     std::vector<std::uint8_t> m_sfMonths;
 
-    std::vector<std::uint8_t> m_months;
+    std::vector<std::uint8_t> m_collapsedOHMonths;
+    std::vector<std::uint8_t> m_collapsedSfMonths;
     std::vector<double> m_OHMonthlyAverages;
     std::vector<double> m_sfMonthlyAverages;
     std::vector<double> m_OHMonthlyStdDevs;
@@ -38,5 +40,6 @@ class OneYear
     void computeSaveSfAverage();
     void computeSaveOHStdDev();
     void computeSaveSfStdDev();
-    void computeMonths();
+    void collapseOHMonths();
+    void collapseSfMonths();
 };
