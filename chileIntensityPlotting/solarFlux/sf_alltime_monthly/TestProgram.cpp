2b289cb9418be310d9ba6aa288aa30c260a867c6
Gabe Decker <42gdeckers@gmail.com>
2025-03-27 15:49:38 -0600

    Added some unit tests to make sure that my monthly average calculations are correct when there is only one present month

--- chileIntensityPlotting/solarFlux/sf_alltime_monthly/TestProgram.cpp
+++ chileIntensityPlotting/solarFlux/sf_alltime_monthly/TestProgram.cpp
@@ -1,3 +1,4 @@
+#include "OneYear.hpp"
 #include "parsing.hpp"
 
 #include <gtest/gtest.h>
@@ -63,4 +64,61 @@ TEST(split, handlesNormal)
     EXPECT_EQ("5839", result[0]);
     EXPECT_EQ("103.53", result[1]);
     EXPECT_EQ("776", result[2]);
+}
+
+TEST(montlyAveragesOneMonth, worksWhenSfOHSame)
+{
+    auto y1 = OneYear("2024",
+                      { 4.2, 3.1, 6.5 },
+                      { 5.2, 1.1, 0.6 },
+                      { 1, 1, 1 },
+                      { 1, 1, 1 });
+    EXPECT_FLOAT_EQ(4.6, y1.getOHAverage()[0]);
+    EXPECT_EQ(1, y1.getOHAverage().size());
+    EXPECT_EQ(1, y1.getOHStdDev().size());
+    EXPECT_FLOAT_EQ(2.3, y1.getSfAverage()[0]);
+    EXPECT_EQ(1, y1.getSfAverage().size());
+    EXPECT_EQ(1, y1.getSfStdDev().size());
+
+    EXPECT_EQ(1, y1.getCollapsedOHMonths().size());
+    EXPECT_EQ(1, y1.getCollapsedOHMonths()[0]);
+    EXPECT_EQ(1, y1.getCollapsedSfMonths().size());
+    EXPECT_EQ(1, y1.getCollapsedSfMonths()[0]);
+}
+
+TEST(montlyAveragesOneMonth, worksWhenSfOHDifferent)
+{
+    auto y1 = OneYear("2024",
+                      { 4.2, 3.1, 6.5 },
+                      { 5.2, 1.1, 0.6 },
+                      { 1, 1, 1 },
+                      { 12, 12, 12 });
+    EXPECT_FLOAT_EQ(4.6, y1.getOHAverage()[0]);
+    EXPECT_EQ(1, y1.getOHAverage().size());
+    EXPECT_EQ(1, y1.getOHStdDev().size());
+    EXPECT_FLOAT_EQ(2.3, y1.getSfAverage()[0]);
+    EXPECT_EQ(1, y1.getSfAverage().size());
+    EXPECT_EQ(1, y1.getSfStdDev().size());
+
+    EXPECT_EQ(1, y1.getCollapsedOHMonths().size());
+    EXPECT_EQ(1, y1.getCollapsedOHMonths()[0]);
+    EXPECT_EQ(1, y1.getCollapsedSfMonths().size());
+    EXPECT_EQ(12, y1.getCollapsedSfMonths()[0]);
+
+    auto y2 = OneYear("2024",
+                      { 4.2, 3.1, 6.5 },
+                      { 5.2, 1.1, 0.6, 5.2, 1.1, 0.6 },
+                      { 1, 1, 1 },
+                      { 10, 10, 10, 10, 10, 10 });
+    EXPECT_FLOAT_EQ(4.6, y1.getOHAverage()[0]);
+    EXPECT_EQ(1, y1.getOHAverage().size());
+    EXPECT_EQ(1, y1.getOHStdDev().size());
+    EXPECT_FLOAT_EQ(2.3, y1.getSfAverage()[0]);
+    EXPECT_EQ(1, y1.getSfAverage().size());
+    EXPECT_EQ(1, y1.getSfStdDev().size());
+
+    EXPECT_EQ(1, y1.getCollapsedOHMonths().size());
+    EXPECT_EQ(1, y1.getCollapsedOHMonths()[0]);
+    EXPECT_EQ(1, y1.getCollapsedSfMonths().size());
+    EXPECT_EQ(12, y1.getCollapsedSfMonths()[0]);
 }
