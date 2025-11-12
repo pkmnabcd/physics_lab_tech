2e41052b4df8d61f175478424ea98312fe403c5a
Gabe Decker <42gdeckers@gmail.com>
2025-11-12 15:34:33 -0700

    Changed the winter total power grapher so that the legend should only have two labels on the legend

--- amtm total power/graphers/Power_graphing_winter.py
+++ amtm total power/graphers/Power_graphing_winter.py
@@ -173,7 +173,9 @@ months2 = ['January', 'February', 'March', 'April']
 mons2 = ['Jan', 'Feb', 'Mar', 'Apr']
 
 color1 = "blue"
+dataLabel1 = f"{year1} Total Power"
 color2 = "red"
+dataLabel2 = f"{year2} Total Power"
 
 title = f"Total Power Winter {year1}-{year2}"
 
@@ -188,14 +190,16 @@ year2_powrs = getPowrForYear(year2, mons2, months2, mainpath)
 
 for i in range(len(year1_powrs)):
     powr = year1_powrs[i]
+    currentLabel = dataLabel1 if i == 0 else "_nolegend_"
 
     lp = len(powr)
-    plt.plot(getTimeInDoY(powr), (powr[0:lp,4]), marker = '.',linestyle = 'solid', markersize = 5, color = f'{color1}', label=f"{year1} Total Power")
+    plt.plot(getTimeInDoY(powr), (powr[0:lp,4]), marker = '.',linestyle = 'solid', markersize = 5, color = f'{color1}', label=currentLabel)
 for i in range(len(year2_powrs)):
     powr = year2_powrs[i]
+    currentLabel = dataLabel2 if i == 0 else "_nolegend_"
 
     lp = len(powr)
-    plt.plot(getTimeInDoY(powr), (powr[0:lp,4]), marker = '.',linestyle = 'solid', markersize = 5, color = f'{color2}', label=f"{year2} Total Power")
+    plt.plot(getTimeInDoY(powr), (powr[0:lp,4]), marker = '.',linestyle = 'solid', markersize = 5, color = f'{color2}', label=currentLabel)
 
 plt.title(title, fontsize=26)
 plt.ylabel('Total power', fontsize=22)
