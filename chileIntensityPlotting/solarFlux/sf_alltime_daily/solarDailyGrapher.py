7abec4f6daaa69ad8ed39d4c90ceda499a72da98
Gabe Decker <42gdeckers@gmail.com>
2025-12-12 22:41:42 -0700

    Finished the solar flux all-time daily grapher

--- chileIntensityPlotting/solarFlux/sf_alltime_daily/solarDailyGrapher.py
+++ chileIntensityPlotting/solarFlux/sf_alltime_daily/solarDailyGrapher.py
@@ -1,74 +1,127 @@
-from sys import argv
-
 import matplotlib.pyplot as plt
+
+def getYeardoy(year, month, dayOfMonth):
+    """
+    Returns the corresponding yeardoy which is the [year].(doy / # days in year)
+    """
+
+    isLeapYear = year % 4 == 0
+    daysInYear = 365
+    if isLeapYear:
+        daysInYear = 366
+
+    dayOfYearAdd = 0
+
+    if month == 1:
+        dayOfYearAdd = 0
+    elif month == 2:
+        dayOfYearAdd = 31
+    elif month == 3:
+        if isLeapYear:
+            dayOfYearAdd = 60
+        else:
+            dayOfYearAdd = 59
+    elif month == 4:
+        if isLeapYear:
+            dayOfYearAdd = 91
+        else:
+            dayOfYearAdd = 90
+    elif month == 5:
+        if isLeapYear:
+            dayOfYearAdd = 121
+        else:
+            dayOfYearAdd = 120
+    elif month == 6:
+        if isLeapYear:
+            dayOfYearAdd = 152
+        else:
+            dayOfYearAdd = 151
+    elif month == 7:
+        if isLeapYear:
+            dayOfYearAdd = 182
+        else:
+            dayOfYearAdd = 181
+    elif month == 8:
+        if isLeapYear:
+            dayOfYearAdd = 213
+        else:
+            dayOfYearAdd = 212
+    elif month == 9:
+        if isLeapYear:
+            dayOfYearAdd = 244
+        else:
+            dayOfYearAdd = 243
+    elif month == 10:
+        if isLeapYear:
+            dayOfYearAdd = 274
+        else:
+            dayOfYearAdd = 273
+    elif month == 11:
+        if isLeapYear:
+            dayOfYearAdd = 305
+        else:
+            dayOfYearAdd = 304
+    elif month == 12:
+        if isLeapYear:
+            dayOfYearAdd = 335
+        else:
+            dayOfYearAdd = 334
+    else:
+        print("WARNING!! The month may not be correct")
+
+    dayOfYear = dayOfYearAdd + dayOfMonth
+    return year + dayOfYear / daysInYear
+
 
 def readAverages(path):
     file = open(path)
     lines = file.readlines()
     for i in range(len(lines)):
         lines[i] = lines[i].strip("\n")
+    file.close()
 
-    ohYearmonths = []  # this is the year and then some multiple of 1/12 to be the month
-    sfYearmonths = []  # this is the year and then some multiple of 1/12 to be the month
-    ohAvgs = []
-    solarAvgs = []
-    ohStdevs = []
-    solarStdevs = []
+    sfYeardoys = []  # this is the year and then some multiple of 1/365 1/366 to be the day of year
+    sfAvgs = []
     for line in lines:
         cols = line.split(",")
 
-        yearmonth = int(cols[0])
-        month = int(cols[1]) - 1
-        yearmonth += (1/12) * month
+        year = int(cols[0])
+        month = int(cols[1])
+        day = int(cols[2])
+        sfAvg = float(cols[3])
 
-        ohAvg = cols[2]
-        if not ohAvg == '':
-            ohAvgs.append(float(ohAvg))
-            ohStdevs.append(float(cols[4]))
-            ohYearmonths.append(yearmonth)
+        sfYeardoys.append(getYeardoy(year, month, day))
+        sfAvgs.append(sfAvg)
 
-        solarAvgs.append(float(cols[3]))
-        solarStdevs.append(float(cols[5]))
-        sfYearmonths.append(yearmonth)
-
-    file.close()
-    return ohYearmonths, sfYearmonths, ohAvgs, solarAvgs, ohStdevs, solarStdevs
+    return sfYeardoys, sfAvgs
 
 
-def makeAndSaveGraph(ohYearmonths, sfYearmonths, ohAvgs, solarAvgs, ohStdevs, averagesPath):
-    fig, ax1 = plt.subplots(figsize=(14,10))
-    #plt.errorbar(times, temps, yerr=stdevs, fmt='o', capsize=5, ecolor="r", elinewidth=.5, label="Daily Average OH Temp")
-
-    ax1.set_xlabel("Year", fontsize=20)
-    ax1.set_ylabel("Solar Flux (SFU)", fontsize=20)
-    ax1.plot(sfYearmonths, solarAvgs, color="red", label="Monthly Average Solar Flux")
-    ax1.tick_params(axis="y", labelcolor="red")
-
-    ax2 = ax1.twinx()
-    ax2.set_ylabel("OH Temp (K)", fontsize=20)
-    ax2.errorbar(ohYearmonths, ohAvgs, yerr=ohStdevs, color="blue", fmt="o", ecolor="purple", label="Monthly Average OH Temp")
-    ax2.tick_params(axis="y", labelcolor="blue")
-
-    fig.tight_layout()
+def makeAndSaveGraph(times, temps, outPath):
+    plt.figure(figsize=(14,10))
     plt.grid(visible=True, axis="both")
 
-    title = "ChileMTM All-Time (2009-2024) Monthly OH Temp and Solar Flux"
+    plt.xlabel("Year", fontsize=22)
+    plt.ylabel("Solar Flux (SFU)", fontsize=22)
+    plt.plot(times, temps, color="blue", label="Daily Average Solar Flux")
+    plt.tick_params(axis="both", labelsize=20)
+
+    plt.tight_layout()
+    plt.grid(visible=True, axis="both")
+
+    title = "2009-2024 Solar Flux Daily Averages"
     plt.title(title, fontsize=26)
 
-    lines1, labels1 = ax1.get_legend_handles_labels()
-    lines2, labels2 = ax2.get_legend_handles_labels()
-    ax2.legend(lines1 + lines2, labels1 + labels2, loc="lower right")
+    plt.legend(fontsize=20)
     plt.tight_layout()
 
-    # NOTE: Assuming averagesPath is the path to ..../all_time_year_averages.csv
-    outPath = averagesPath.replace(".csv", ".png")
     plt.savefig(outPath)
     print(f"File saved to {outPath} .")
 
 
 if __name__ == "__main__":
-    averagesPath = "all_time_oh_sf_month_averages.csv"
-    ohYearmonths, sfYearmonths, ohAvgs, solarAvgs, ohStdevs, solarStdevs = readAverages(averagesPath)
+    averagesPath = "all_time_sf_daily_averages.csv"
+    outPath = "all_time_sf_daily_average.png"
+    sfYeardoys, sfAvgs = readAverages(averagesPath)
 
-    makeAndSaveGraph(ohYearmonths, sfYearmonths, ohAvgs, solarAvgs, ohStdevs, averagesPath)
+    makeAndSaveGraph(sfYeardoys, sfAvgs, outPath)
 
