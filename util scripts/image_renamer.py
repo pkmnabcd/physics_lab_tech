df326a95e01e9f86a611631ea81e80d4db49f00d
Gabe Decker <42gdeckers@gmail.com>
2026-01-06 13:22:07 -0700

    Made it so all processed and raw data gets copied and saved

--- util scripts/image_renamer.py
+++ util scripts/image_renamer.py
@@ -19,26 +19,29 @@ def getMatchingFiles(pattern, all_files):
 
 def main():
     path = "./data"  # TODO: change this to "." once I'm done testing
-    all_files = [f for f in listdir(path) if isfile(join(path, f))]
+    processed_path = join(path, "Processed")
+    all_raw_files = [f for f in listdir(path) if isfile(join(path, f))]
+    all_processed_files = [f for f in listdir(processed_path) if isfile(join(processed_path, f))]
 
     tmp_folder_path = join(path, "tmp_backup")
+    tmp_folder_path_processed = join(tmp_folder_path, "Processed")
     if exists(tmp_folder_path):
         return  # NOTE: don't want to overwrite existing copies or redo the renaming
 
     mkdir(tmp_folder_path)
+    mkdir(tmp_folder_path_processed)
 
     raw_file_patterns = ["BG_31_[0-9]{4}.tif", "P12_31_[0-9]{4}.tif", "P14_31_[0-9]{4}.tif"]
-    raw_bg_files = getMatchingFiles(raw_file_patterns[0], all_files)
-    raw_p12_files = getMatchingFiles(raw_file_patterns[1], all_files)
-    raw_p14_files = getMatchingFiles(raw_file_patterns[2], all_files)
+    raw_bg_files = getMatchingFiles(raw_file_patterns[0], all_raw_files)
+    raw_p12_files = getMatchingFiles(raw_file_patterns[1], all_raw_files)
+    raw_p14_files = getMatchingFiles(raw_file_patterns[2], all_raw_files)
 
     print("Making copies of the raw data")
-    for file in raw_bg_files:
+    for file in all_raw_files:
         copy2(join(path, file), tmp_folder_path)
-    for file in raw_p12_files:
-        copy2(join(path, file), tmp_folder_path)
-    for file in raw_p14_files:
-        copy2(join(path, file), tmp_folder_path)
+    print("Making copies of the processed data")
+    for file in all_processed_files:
+        copy2(join(processed_path, file), tmp_folder_path_processed)
 
 
 main()
