021aae8db00fa0f1d19a3343e613ff2074c3f69f
Gabe Decker <42gdeckers@gmail.com>
2026-02-19 13:09:29 -0700

    Added code to try to compile an IDL file.

--- amtm total power/runners/power_spectrum_runner.py
+++ amtm total power/runners/power_spectrum_runner.py
@@ -9,3 +9,15 @@ sys.path.append(f"{IDL_DIR}/lib/bridges")
 from idlpy import *
 
 IDL.print("Hello world!")
+
+
+FFT_FILENAME = "m_fft_asi.pro"
+READ_IMAGE_FILENAME = "read_images_ALOMAR.pro"
+SHELLRUNNER_FILENAME = "mlshellrunnertest.pro"
+
+
+# NOTE: the following code yields the path
+# C:\Users\Domi\OneDrive\Desktop\MachineLearning\IDLCode
+idl_scripts_dir = join("C:/", "Users", "Domi", "OneDrive", "Desktop", "MachineLearning", "IDLCode")
+
+IDL.run(f".compile {join(idl_scripts_dir, FFT_FILENAME)}")
