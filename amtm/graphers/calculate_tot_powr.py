13eab35b2c8e2af4d45d0e074bfb4092ae0503e0
Gabe Decker <42gdeckers@gmail.com>
2026-03-10 15:42:59 -0600

    Added a function to get the index of the window we're looking at. Added the remaining code that should be needed for the calculations

--- amtm/graphers/calculate_tot_powr.py
+++ amtm/graphers/calculate_tot_powr.py
@@ -63,17 +63,29 @@ def getDaysTxtData(days_path):
         data.append((line[0], int(line[1]), int(line[2])))
     return data
 
+
+def getWindowIndex(days_data, night, begin, end):
+    window_found = False
+    index = -1
+    for i in range(len(days_data)):
+        if days_data[i][0] == night and days_data[i][1] == begin and days_data[i][2] == end:
+            index = i
+            window_found = True
+            break
+
+    return window_found, index
+
+
+# NOTE: make sure begin and end are strings
 def calcWindowTotalPowerOverTime(year, month, mon, night, begin, end, mainpath):
     dayframe = f'{mon}{night}_{begin}-{end}'
 
     days_path = join(mainpath, year, f'{month}{year}', 'days.txt')
     timestamp_path = join(mainpath, year, f'{month}{year}', 'timestamp.txt')
-    save_location = join(mainpath, year, f'{month}{year}', dayframe, "T_and_power.txt")
+    save_path = join(mainpath, year, f'{month}{year}', dayframe, "T_and_power.txt")
 
     days_data = getDaysTxtData(days_path)
     timestamp_data = np.loadtxt(timestamp_path)
-    # TODO: Add code to make sure days.txt and timestamp.txt have the same amount of data
-    # Look for the given day's index in the timestamp data
 
     if timefile.ndim == 1:
         timefile = np.array([timefile])  # This covers when np.loadtxt automatically reshapes single-row files
@@ -82,8 +94,6 @@ def calcWindowTotalPowerOverTime(year, month, mon, night, begin, end, mainpath):
         print(f"WARNING: The length of days.txt: {len(days_data)} does not equal the length of timestamp.txt: {len(timestamp_data)}")
         return
 
-    tL = len(timefile) # Length of the columns in the file
-
     myear = timefile[:,0]
     mmonth = timefile[:,1]
     mday = timefile[:,2]
@@ -91,6 +101,7 @@ def calcWindowTotalPowerOverTime(year, month, mon, night, begin, end, mainpath):
     mmin = timefile[:,4]
     msec = timefile[:,5]
 
+    tL = len(timefile) # Length of the columns in the file
     # creates a numpy array containing the starting time in decimal hours
     tmp_dec_hour_holder = []
     for i in range(0,tL):
@@ -98,6 +109,79 @@ def calcWindowTotalPowerOverTime(year, month, mon, night, begin, end, mainpath):
         tmp_dec_hour_holder.append(replace)
     dec_hour = np.array(tmp_dec_hour_holder)
 
+    found, window_index = getWindowIndex(days_data, night, begin, end)
+    if not found:
+        print(f"WARNING!! The window {mon}{night} [{begin},{end}] was not found in days.txt. Make sure everything in days.txt is present and formatted correctly.")
+        return
+
+    dyear = myear[window_index]
+    dmonth = mmonth[window_index]
+    day = mday[window_index]
+    hour = dec_hour[window_index]
+
+    perd_power = []
+    perd_exponent = []
+    perd_timelist = []
+    perd_y, perd_m, perd_d, perd_df = [],[],[], []
+    perd_timelist = []
+    perd_power = []
+    perd_exponent = []
+
+    # Iterating over each possible TempOH _TOTAL.csv file
+    for i in range(0,50):
+        csv_path = join(main_path, year, f"{month}{year}", dayframe, f"TempOH{i}_TOTAL.csv")
+        if not exists(csv_path):
+            break
+        tally = hour + i*0.5 #Gets the time associated with the power file
+
+        perd_timelist.append(tally)
+        perd_y.append(dyear)
+        perd_m.append(dmonth)
+        perd_d.append(day)
+        perd_df.append(dayframe)
+
+        #creating an empty 301x301 array to import the csv file to.
+        array1 = np.empty([L1,L2])
+        rows = [] # creating an empty list to hold csv row
+        with open(csv_path, 'r') as file: #opening and reading csv
+            datafile = csv.reader(file)
+            for row in datafile:         #taking each row of the csv and putting into
+                rows.append(row)         # a list, which results in a nested list
+
+            # Takes each element that is not a value of -22 of the nested list (row[i][j]
+            # and places it the cooresponding array location (array[i,j]), the values of
+            # -22 are replaced by zero,
+
+            for i in range(0,L1):
+                for j in range(0,L2):
+                    value = float(rows[i][j])
+                    if -22 != value:
+                        #Takes value from txt file and makes it the power 10 it is raised to
+                        array1[i,j] = 10**value
+                    elif -22 == value:
+                        array1[i,j] = 0
+
+        powerval = np.sum(array1) #this sum gives the total power
+        perd_power.append(powerval)
+        perd_exponent.append(math.log10(powerval))
+
+
+    perd_yrs = np.array(perd_y)
+    perd_monts = np.array(perd_m)
+    perd_days = np.array(perd_d)
+    perd_hrs = np.array(perd_timelist)
+    perd_powr = np.array(perd_power)
+    perd_exp = np.array(perd_exponent)
+
+    # Make a T_and_power.txt file for each day
+    perday_datetime = np.column_stack([perd_yrs,perd_monts, perd_days, perd_hrs, perd_powr, perd_exp])
+
+    words = 'Total power for every half-hour of avalible data\n of'\
+        f' {mon} {year} {day} where the columns are 0-year 1-month 2-day 3-time in decimal hour\n'\
+            '4- power value(x) 5-exponent(y) of power in base 10'
+
+    np.savetxt(save_path, perday_datetime, fmt ='%4d %2d %2d %1.6f %1.20f %1.5f'\
+                ,header = words, comments = '#')
 
 
 
